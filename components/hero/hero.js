1d15070680bbf546e2d0bfbf9793f954d80891d7
Susan <147873804+theelusivegecko@users.noreply.github.com>
2025-03-19 15:06:24 -0400

    SCSS restructuring, added fonts, basic variables

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -4,12 +4,19 @@ const Hero = () => {
 	return (
 		<section className={styles.section_Hero}>
 			<div className={`${styles.container_Hero} ${" container"}`}>
-				<h1>Strengthening Community-Based Care, Together</h1>
-				<a
-					className="btn btn-blue"
-					href="#"
-				>Learn more about joining CINQCARE</a>
-				
+				<div>
+					<h1><em>Strengthening</em> Community-Based Care, Together</h1>
+					<a
+						className="btn btn-blue"
+						href="#"
+					>Learn more about joining CINQCARE</a>
+ 				</div>
+				<div>
+					<blockquote>
+						“CINQCARE isn’t just another network. They truly walk alongside providers, giving us the support we need to succeed in value-based care while keeping patient relationships at the center.”
+						<span>- Dr. Wilfred Herard, MD — CEO of Proactive Care IPA</span>
+					</blockquote>
+				</div>
 			</div>
 		</section>
 	);
--- pages/providers.js
+++ pages/providers.js
@@ -12,8 +12,8 @@ export default function Providers() {
       </Head>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <h1>For Providers</h1>
           <Hero />
+          
         </main>
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
 
