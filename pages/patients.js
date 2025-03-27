f4c20d9aaa7ff64e306e9666673d2f1445a4cb26
Susan <147873804+theelusivegecko@users.noreply.github.com>
2025-03-27 17:23:34 -0400

    Providers, patients, partners loose content layout

--- pages/patients.js
+++ pages/patients.js
@@ -3,9 +3,15 @@ import OpengraphImg from "../public/cinqcare-opengraph.jpg";
 import Image from "next/image";
 import Header from "../components/header/header";
 import Hero from "../components/hero/hero";
-import Footer from "../components/footer/footer";
-import CTA from "../components/cta/cta";
+import ScrollJacking from "../components/scrollJacking/scrollJacking";
+import circle from "/public/circle.svg";
+import IconBlocks from "../components/iconBlocks/iconBlocks";
+import TestimonialSlider from "../components/testimonialSlider/testimonialSlider";
 import Map from "../components/map/map";
+import GlassAccordion from "../components/glassAccordion/glassAccordion";
+import CTA from "../components/cta/cta";
+import Accordion from "../components/accordion/accordion";
+import Footer from "../components/footer/footer";
 
 export default function Patients() {
 
@@ -35,9 +41,33 @@ export default function Patients() {
       <main>
         <Hero page={currentPage} />
 
+        <ScrollJacking page={currentPage} bg={circle}>
+          <div>
+            <h2>What if we made delivering care a <em>little easier</em> for the providers who know their communities best?</h2>
+          </div>
+          <div>
+            <h2>You're not just <em>treating symptoms</em> — you’re navigating a system that wasn’t built for high-needs, underserved populations.</h2>
+          </div>
+          <div>
+            <h2>Administrative burdens grow, care models shift, and reimbursements remain unpredictable. <em>Yet, your commitment never wavers.</em></h2>
+          </div>
+        </ScrollJacking>
+        <IconBlocks page={currentPage} />
         <Map page={currentPage} />
-
-        <CTA />
+        <TestimonialSlider page={currentPage} />
+        <GlassAccordion page={currentPage} />
+        <CTA
+          type="primary"
+          buttonText="Contact Us Today"
+          link="/"
+          target="_blank"
+        >
+          <header>
+            <h2 class="period">We want to hear <em>from you</em></h2>
+          </header>
+          <p>Tell us about your community and your good work. If it’s your calling to deliver care where you live, we are ready to help.</p>
+        </CTA>
+        <Accordion page={currentPage} />
       </main>
 
       <Footer />
