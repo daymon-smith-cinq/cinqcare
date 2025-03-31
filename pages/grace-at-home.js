f174ac29ade675dea99c24ad30a1ef1dedb74c16
red-slime <xaneslime@gmail.com>
2025-03-31 15:26:45 -0400

    heroAlt + black outline for button and additional hover states for dark header

--- pages/grace-at-home.js
+++ pages/grace-at-home.js
@@ -12,63 +12,73 @@ import Masonry from "../components/masonry/masonry";
 import Footer from "../components/footer/footer";
 
 export default function GraceAtHome() {
+	// Define current page
+	const currentPage = "grace-at-home";
 
-  // Define current page
-  const currentPage = "grace-at-home";
+	// SEO
+	const title = "Grace at Home | CINQCARE";
+	const metaDescription =
+		"Grace at Home brings expert primary, urgent, and chronic care to you. Quality health support tailored to your needs, right at home.";
+	const pageURL = "https://cinq.care/grace-at-home";
 
-  // SEO
-  const title = "Grace at Home | CINQCARE";
-  const metaDescription = "Grace at Home brings expert primary, urgent, and chronic care to you. Quality health support tailored to your needs, right at home.";
-  const pageURL = "https://cinq.care/grace-at-home";
+	return (
+		<>
+			<Head>
+				<meta name="description" content={metaDescription} />
+				<meta property="og:image" content={OpengraphImg.src} />
+				<meta property="og:title" content={title} />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={pageURL} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@carecinq" />
+				<title>{title}</title>
+			</Head>
 
-  return (
-    <>
-      <Head>
-        <meta name="description" content={metaDescription} />
-        <meta property="og:image" content={OpengraphImg.src} />
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={pageURL} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@carecinq" />
-        <title>{title}</title>
-      </Head>
+			<Header headerBg="light" />
 
-      <Header headerBg="dark" />
+			<main>
+				<HeroAlt page={currentPage} />
+				<OrangeBox page={currentPage} />
+				<Map page={currentPage} />
+				<SideBySide page={currentPage} num="first" />
+				<IconBlocks page={currentPage} />
+				<SideBySide page={currentPage} num="second" />
+				<CTA
+					type="secondary"
+					buttonText="Contact Us Today"
+					link="/"
+					target="_blank"
+				>
+					<header>
+						<h2 class="period">
+							We want to hear <em>from you</em>
+						</h2>
+					</header>
+					<p>
+						Tell us about your community and your good work. If it’s your
+						calling to deliver care where you live, we are ready to help.
+					</p>
+				</CTA>
+				<Masonry page={currentPage} />
+				<CTA
+					type="secondary"
+					buttonText="Contact Us Today"
+					link="/"
+					target="_blank"
+				>
+					<header>
+						<h2 class="period">
+							We want to hear <em>from you</em>
+						</h2>
+					</header>
+					<p>
+						Tell us about your community and your good work. If it’s your
+						calling to deliver care where you live, we are ready to help.
+					</p>
+				</CTA>
+			</main>
 
-      <main>
-        <HeroAlt page={currentPage} />
-        <OrangeBox page={currentPage} />
-        <Map page={currentPage} />
-        <SideBySide page={currentPage} num="first" />
-        <IconBlocks page={currentPage} />
-        <SideBySide page={currentPage} num="second" />
-        <CTA
-          type="secondary"
-          buttonText="Contact Us Today"
-          link="/"
-          target="_blank"
-        >
-          <header>
-            <h2 class="period">We want to hear <em>from you</em></h2>
-          </header>
-          <p>Tell us about your community and your good work. If it’s your calling to deliver care where you live, we are ready to help.</p>
-        </CTA>
-        <Masonry page={currentPage} />
-        <CTA
-          type="secondary"
-          buttonText="Contact Us Today"
-          link="/"
-          target="_blank"
-        >
-          <header>
-            <h2 class="period">We want to hear <em>from you</em></h2>
-          </header>
-          <p>Tell us about your community and your good work. If it’s your calling to deliver care where you live, we are ready to help.</p>
-        </CTA>
-      </main>
-
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 }
