f8243c76d22cc08d54916147ee0e9b02f776a5e9
red-slime <xaneslime@gmail.com>
2025-03-31 12:35:29 -0400

    changes to iconBlocks and hero, included careers

--- pages/careers.js
+++ pages/careers.js
@@ -3,41 +3,40 @@ import OpengraphImg from "../public/cinqcare-opengraph.jpg";
 import Image from "next/image";
 import Header from "../components/header/header";
 import Hero from "../components/hero/hero";
+import JobBoard from "@/components/jobBoard/jobBoard";
 import Footer from "../components/footer/footer";
-import CTA from "../components/cta/cta";
 
 export default function Careers() {
+	// Define current page
+	const currentPage = "careers";
 
-  // Define current page
-  const currentPage = "careers";
+	// SEO
+	const title = "Careers | CINQCARE";
+	const metaDescription =
+		"Join CINQCARE’s mission to deliver health and care to high-needs communities. Answer the call to make a difference—explore careers today.";
+	const pageURL = "https://cinq.care/careers";
 
-  // SEO
-  const title = "Careers | CINQCARE";
-  const metaDescription = "Join CINQCARE’s mission to deliver health and care to high-needs communities. Answer the call to make a difference—explore careers today.";
-  const pageURL = "https://cinq.care/careers";
+	return (
+		<>
+			<Head>
+				<meta name="description" content={metaDescription} />
+				<meta property="og:image" content={OpengraphImg.src} />
+				<meta property="og:title" content={title} />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={pageURL} />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@carecinq" />
+				<title>{title}</title>
+			</Head>
 
-  return (
-    <>
-      <Head>
-        <meta name="description" content={metaDescription} />
-        <meta property="og:image" content={OpengraphImg.src} />
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={pageURL} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@carecinq" />
-        <title>{title}</title>
-      </Head>
+			<Header headerBg="dark" />
 
-      <Header headerBg="dark" />
+			<main>
+				<Hero page={currentPage} />
+				<JobBoard />
+			</main>
 
-      <main>
-        <Hero page={currentPage} />
-
-        <CTA />
-      </main>
-
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 }
