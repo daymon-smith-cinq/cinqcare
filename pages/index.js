901897fe2d29e41adbcd817ab97cb5fea79eab31
daymon-smith-cinq <daymon.smith@cinq.care>
2025-05-19 14:07:03 -0500

    Update index.js
    Adding Google Tag Manager

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,14 @@ export default function Home() {
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:site" content="@carecinq" />
 				<title>{title}</title>
+
+			<!-- Google Tag Manager -->
+				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-PXSVWVJS');</script>
+			<!-- End Google Tag Manager -->
 			</Head>
 
 			<Header headerBg="dark" />
